226b3d6ea93d7fc6e46acc28d10393ee57e1b575
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2023-10-10 22:16:25 -0300

    creating game logic

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,12 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Hangman extends JFrame implements ActionListener {
     private final String[] words = {"hangman", "java", "swing", "programming", "openai", "computer", "laptop"};
     private String wordToGuess;
-    private final int guessesLeft = 6;
+    private int guessesLeft = 6;
     private StringBuilder hiddenWord;
+    private String victoryMessage = "Congratulations! You won!";
+    private String defeatMessage = String.format("You lose! The word was: %s", wordToGuess);
 
     private JFrame gameFrame;
     private JLabel hiddenWordLabel;
@@ -41,13 +44,81 @@ public class Hangman extends JFrame implements ActionListener {
 
         gameFrame.getContentPane().add(mainPanel);
 
+        initializeGame();
+
         gameFrame.pack();
         gameFrame.setLocationRelativeTo(null);
         gameFrame.setVisible(true);
     }
 
+    public void initializeGame() {
+        Random random = new Random();
+
+        int selectedWordPosition = random.nextInt(0, words.length);
+        wordToGuess = words[selectedWordPosition];
+        hiddenWord = new StringBuilder();
+
+        for (int i = 0; i < wordToGuess.length(); i++)  {
+            hiddenWord.append("_ ");
+        }
+        hiddenWordLabel.setText(hiddenWord.toString());
+        resetNumberOfGuesses();
+    }
+
+    public void resetNumberOfGuesses() {
+        guessesLeft = 6;
+        guessesLeftLabel.setText(String.format("Guesses left: %s", String.valueOf(guessesLeft)));
+    }
+
+    public void updateHiddenWord(char guess) {
+        boolean charLocated = false;
+
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.charAt(i) == guess) {
+                hiddenWord.setCharAt(i, guess);
+                charLocated = true;
+            }
+        }
+
+        hiddenWordLabel.setText(hiddenWord.toString());
+        verifyGuess(charLocated);
+    }
+
+    public void verifyGuess(boolean guess) {
+        if (!guess) {
+            guessesLeft--;
+            guessesLeftLabel.setText(String.format("Guesses left: %s", String.valueOf(guessesLeft)));
+            verifyGameOver(guessesLeft);
+        } else if (hiddenWord.toString().equals(wordToGuess)) {
+            gameOver(victoryMessage);
+        }
+    }
+
+    public void verifyGameOver(int numberOfGuesses) {
+        if (numberOfGuesses == 0) {
+            gameOver(defeatMessage);
+        }
+    }
+
+    public void gameOver(String message) {
+        guessTextField.setEnabled(false);
+        guessButton.setEnabled(false);
+        JOptionPane.showMessageDialog(this, message, "Game Over", JOptionPane.INFORMATION_MESSAGE);
+        initializeGame();
+        guessTextField.setEnabled(true);
+        guessButton.setEnabled(true);
+        guessTextField.requestFocus();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == guessButton || e.getSource() == guessTextField) {
+            String guessText = guessTextField.getText();
+            if (guessText.length() > 0) {
+                char guess = guessText.charAt(0);
+                updateHiddenWord(guess);
+                guessTextField.setText("");
+            }
+        }
     }
 }
